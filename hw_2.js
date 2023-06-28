bcbcfccfc3cb252fe469b5147e62b45d5c9d0566
Yana <miriel.yana@gmail.com>
2023-06-28 16:07:07 +0300

    changes has been made

--- hw_2.js
+++ hw_2.js
@@ -1,3 +1,8 @@
+//  homework 2. ДЗ 2. Перетворення зі string у number. 
+// 1. За допомогою функції prompt запитати у користувача ширину блока у пікселях (наприклад: 100px) і отримати це значення\записати у змінну.
+// 2. Перетворити це значення у тип number та вивести у конслоль 
+
+console.log('ДЗ 2. Перетворення зі string у number');
 
 const widthBlock = prompt("What is the block's width in pixels?");
 console.log(typeof(widthBlock)); // not necessarily, just to check a behavior
