df17f2afab2a5280452e0a93794c529c65534bd4
Yana <miriel.yana@gmail.com>
2023-07-22 18:25:02 +0300

    Edited

--- hw_11.js
+++ hw_11.js
@@ -4,14 +4,6 @@
 // Нумерація починається з 1. Зверніть увагу на двокрапку та пробіл між ними.
 
 
-// const orderedArray = (array) => {
-//     const newArray = [];
-//     array.forEach((element, index) => {
-//         newArray.push(`${index+1}: ${element}`);
-//     });
-//     console.log(newArray)
-// }
-
 const orderedArray = (array) => {
     const res = (array.map((item, index) => (`${index + 1}: ${item}`)));
     console.log(res);
